044bf9aeee3726442ec2c5867ed28ab8f749852d
eugenehan94 <eugene.han94@gmail.com>
2022-06-02 18:28:24 -0400

    added types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = "Traversy Media";
+let isPublished = true;
+let x = "Hello";
+let age;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+// Tuple
+let person = [1, "Brad", true];
+// Tuple Array
+let employee;
+employee = [[1, "Brad"], [2, "John"], [3, "Jill"]];
+// Union
+let pid = 22;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+// Basic Types
 let id: number = 5
+let company: string = "Traversy Media"
+let isPublished: boolean = true
+let x: any = "Hello"
+let age: number
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[]=[1, true, "Hello"]
+
+// Tuple
+let person: [number, string, boolean] =[1, "Brad", true]
+// Tuple Array
+let employee: [number, string][]
+employee = [[1,"Brad"], [2, "John"], [3, "Jill"]]
+
+// Union
+let pid: string | number = 22
